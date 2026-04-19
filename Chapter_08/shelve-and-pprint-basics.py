93b0dc91edd800a5193891221173d3d453425158
apatheic <apatheic@proton.me>
2026-04-19 13:33:27 +0400

    Create shelve-and-pprint-basics.pyи

---
+++ Chapter_08/shelve-and-pprint-basics.py
@@ -0,0 +1,7 @@
+import shelve, pprint
+
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pokka', 'desc': 'floffy'}]
+print(pprint.pformat(cats))
+fileObj = open('myCats', 'w')
+print(fileObj.write(f'cats = {pprint.pformat(cats)} \n'))
+fileObj.close()
