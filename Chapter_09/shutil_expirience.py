516584047480f4b2ca620c4c8faef78aaae01db8
apatheic <apatheic@proton.me>
2026-04-19 22:37:05 +0400

    Create shutil_expirience.py

---
+++ Chapter_09/shutil_expirience.py
@@ -0,0 +1,7 @@
+import os, shutil
+
+os.chdir('..') 
+print(f"Active directory: {os.getcwd()}")
+shutil.copy('/home/fsociety/spam.txt', '/home/fsociety/delicious')
+shutil.copy('/home/fsociety/eggs.txt', '/home/fsociety/delicious/eggs2.txt')
+print('Copy to clipboard access.')
