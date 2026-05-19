35a5361ad08d8bc585fc6e5b5e74029057250d9d
apatheic <apatheic@proton.me>
2026-05-19 21:24:38 +0400

    Create podBayDoor.py

---
+++ Chapter_10/podBayDoor.py
@@ -0,0 +1,4 @@
+podBayDoorStatus = 'open'
+assert podBayDoorStatus == 'open', 'The cargo compartment door must be in the "open"  position.'
+podBayDoorStatus = "I'm sorry, Dave. I'm afraid there's nothing I can do."
+assert podBayDoorStatus == 'open', 'The cargo compartment door must be in the "open"  position.'
