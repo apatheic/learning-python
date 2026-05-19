d1bec115128ad92ae595e7c02b6628cb8bab9ed2
apatheic <apatheic@proton.me>
2026-05-20 03:00:16 +0400

    Update allErrors.py

--- Chapter_10/allErrors.py
+++ Chapter_10/allErrors.py
@@ -1,5 +1,6 @@
 import logging
 logging.basicConfig(level = logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+#logging.disable(logging.INFO)
 logging.debug('Debugging information.')
 logging.info('Logging module works.')
 logging.warning('Risk of receiving an error message')
