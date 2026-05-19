b14d827502e9eacd90e157ac8490d46a5c28f3ad
apatheic <apatheic@proton.me>
2026-05-20 01:30:41 +0400

    Update boxPrint.py

--- Chapter_10/boxPrint.py
+++ Chapter_10/boxPrint.py
@@ -1,17 +1,17 @@
 def boxPrint(symbol, width, height):
-  if len(symbol) != 1:
-    raise Exception("Variable symbol must be single character.")
-  if width <= 2:
-    raise Exception('Value width must exceed 2.')
-  if height <= 2:
-    raise Exception('Value height must exceed 2.')
-  print(symbol * width)
+    if len(symbol) != 1:
+        raise Exception("Variable symbol must be single character.")
+    if width <= 2:
+        raise Exception('Value width must exceed 2.')
+    if height <= 2:
+        raise Exception('Value height must exceed 2.')
+    print(symbol * width)
 
-  for i in range(height - 2):
-    print(symbol + (" " * (width - 2)) + symbol)
-  print(symbol * width)
-for sym, w, h in (("*", 4, 4), ('O',20, 5), ("x", 1, 3), ('ZZ', 3, 3)):
-  try:
-    boxPrint(sym, w, h)
-  except Exception as err:
-    print(f'An exception was thrown {err}')
+    for i in range(height - 2):
+        print(symbol + " " * (width - 2) + symbol)
+    print(symbol * width)
+for sym, w, h in [('&', 10, 10)]:
+    try:
+        boxPrint(sym, w, h)
+    except Exception as error:
+        print(f'We found a trouble: {error}')
