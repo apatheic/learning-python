7117542f43b29a4f3bb9a995544955cc07a3756c
apatheic <apatheic@proton.me>
2026-05-21 00:01:14 +0400

    Update BrowserSelenium.py

--- Chapter_11/BrowserSelenium.py
+++ Chapter_11/BrowserSelenium.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-type(browser)
-#<class 'selenium.webdriver.firefox.webdriver.WebDriver'>
-
 browser.get('http://inventwithpython.com')
+
+try:
+    elem = browser.find_element_by_class_name('bookcover')
+    print('element with <%s>  the given class name found!' %
+          (elem.tag_name))
+except:
+    print('No element with the given class name could be found.')
