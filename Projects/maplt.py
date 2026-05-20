da181725be6efb45bbb0fd42d5ac813924354858
apatheic <apatheic@proton.me>
2026-05-20 16:15:33 +0400

    Create maplt.py

---
+++ Projects/maplt.py
@@ -0,0 +1,8 @@
+import webbrowser, sys, pyperclip
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+    
+webbrowser.open(f'https://www.google.com/maps/place/{address}')
