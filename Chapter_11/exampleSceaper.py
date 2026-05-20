ee662fdd9d02393e2231d7d15c6a7debdfc67cf4
apatheic <apatheic@proton.me>
2026-05-20 22:47:01 +0400

    Create exampleSceaper.py

---
+++ Chapter_11/exampleSceaper.py
@@ -0,0 +1,23 @@
+import bs4
+
+exampleFile = open("example.html")
+exampleSoup = bs4.BeautifulSoup(exampleFile.read())
+elems = exampleSoup.select('#author')
+
+type(elems)
+#<class'list'>
+
+len(elems)
+#1
+
+type(elems[0])
+#<class 'bs4.element.Tag'>
+
+elems[0].getText()
+#'Al Savigart'
+
+str(elems[0])
+#<span id="author">Al Savigart</span>
+
+elems[0].attrs
+#{'id': 'author'}
