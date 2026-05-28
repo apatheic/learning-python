e705b058367106937748f81025efe9557d232338
apatheic <apatheic@proton.me>
2026-05-28 19:40:42 +0400

    Update ReaderCSV.py

--- Chapter_14/ReaderCSV.py
+++ Chapter_14/ReaderCSV.py
@@ -1,15 +1,20 @@
 import csv
 
 #opening file
-exampleFile = open('example.csv')
+with open('example.csv', 'r', encoding ='utf8') as f:
 
-#call object Reader
-exampleReader = csv.reader(exampleFile)
+    #call object Reader
+    exampleReader = csv.reader(f)
 
-#Convert to a regular Python list
-exampleData = list(exampleReader)
+    #Convert to a regular Python list
+    exampleData = list(exampleReader)
 
-print(exampleData)
-#[['Timestamp', 'Fruit', 'Quantity'], ['4/5/2025 13:34', 'Apples', '73'], ['4/5/2025 3:41', 'Cherries', '85'], ['4/6/2025 12:46', 'Pears', '14'], ['4/8/2025 8:59', 'Oranges', '52'], ['4/10/2025 2:07', 'Apples', '152'], ['4/10/2025 18:10', 'Bananas', '23'], ['4/10/2025 2:40', 'Strawberries', '98']]
+    print(exampleData)
+    #[['Timestamp', 'Fruit', 'Quantity'], ['4/5/2025 13:34', 'Apples', '73'], ['4/5/2025 3:41', 'Cherries', '85'], ['4/6/2025 12:46', 'Pears', '14'], ['4/8/2025 8:59', 'Oranges', '52'], ['4/10/2025 2:07', 'Apples', '152'], ['4/10/2025 18:10', 'Bananas', '23'], ['4/10/2025 2:40', 'Strawberries', '98']]
 
-exampleFile.close()
+    print(exampleData[1][0])
+    #4/5/2025 13:34
+    print(exampleData[1][1])
+    #Apples
+    print(exampleData[1][2])
+    #73
