685164aea7a8b1b6ee32656907d0730fe5f5a260
apatheic <apatheic@proton.me>
2026-06-03 23:08:38 +0400

    Update mouseNow.py

--- Projects/mouseNow.py
+++ Projects/mouseNow.py
@@ -2,11 +2,14 @@
 
 import pyautogui
 print('For exit press keys <Ctrl+C>.')
-
 try:
     while True:
         x, y = pyautogui.position()
         positionStr = f'X: {str(x).rjust(4)} Y: {str(y).rjust(4)}'
+        pixelColor = pyautogui.screenshot().getpixel((x, y))
+        positionStr += f' RGB: ({str(pixelColor[0]).rjust(3)}'
+        positionStr += f', {str(pixelColor[1]).rjust(3)}'
+        positionStr += f', {str(pixelColor[2]).rjust(3)})'
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 
