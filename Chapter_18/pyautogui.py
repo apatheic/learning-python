584604c10238c4dbcc6ad85bc8c65bef61088662
apatheic <apatheic@proton.me>
2026-06-03 20:52:38 +0400

    Create pyautogui.py

---
+++ Chapter_18/pyautogui.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+pyautogui.size()
+#1920x1080
+width, height = pyautogui.size()
+
+for i in range(10):
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
