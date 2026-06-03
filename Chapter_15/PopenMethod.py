e6b5dfd2973a7cd3e01631be2df1b4bbe2ef2dc5
apatheic <apatheic@proton.me>
2026-06-03 16:58:54 +0400

    Update PopenMethod.py

--- Chapter_15/PopenMethod.py
+++ Chapter_15/PopenMethod.py
@@ -7,3 +7,9 @@ calcProc.wait()
 #0
 calcProc.poll()
 #0
+
+with open('hello.txt', 'w') as file:
+    file.write("Hello, World!")
+    #12
+
+subprocess.Popen(['cat', 'hello.txt'], shell=True) #also instead 'see' we can use 'start' for Windows, and 'open' for OS X
